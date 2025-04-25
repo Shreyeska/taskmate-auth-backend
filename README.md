8b00faf3b561e98956d3fef07e51088d0198d95d
Shreyeska <129605801+Shreyeska@users.noreply.github.com>
2025-04-25 12:25:12 +0545

    Intiated backend project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# taskmate-auth-backend" 
+Steps for backend. 
+
+
+1. Initialized github repo and cloned
+2. npm init -y
+3. package.json -> made "type": "module"
+4. npm i nodemon express cors bcrypt dotenv
+5. made index.js file
+6. package.json -> "start":"nodemon index.js"
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express ();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const port =process.env.port
+
+app.get("/",(req,res) => {
+    res.send("Hello World!");
+}
+);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
